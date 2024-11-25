7236c3bf3b5dad01d3d94c8edf9bf35258132402
LeonardoArantes <64928255+LeonardoArantes@users.noreply.github.com>
2024-11-25 18:31:34 +0000

    initial Kafka Implementation,, needs testing

---
+++ microsservice-db/src/main/java/com/microsservice_db/Consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.microsservice_db.Consumer;
+
+@Service
+public class KafkaConsumer {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Value("${spting.kafka.consumer.group-id}")
+    private String groupId;
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}",
+                    groupId = "${spting.kafka.consumer.group-id}")
+    public void consume(String message) {
+        System.out.println("Kafka consumer belongs to group: {} received a message {} topic {}", 
+                           groupId, message, topicName);
+    }
+}
